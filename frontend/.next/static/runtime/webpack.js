6be9faecb4936530e1676e58717dfe2a9245ee30
Tristan Stutzman <tstutzman@beardlysoft.com>
2019-08-07 14:03:32 -0700

    initial commit

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eaa2e5ecff25e8bfa02d";
+/******/ 	var hotCurrentHash = "bc372a02518b591b15dc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ frontend/.next/static/webpack/static/development/pages/update.js.7fd6a9a078dbb2313741.hot-update.js
@@ -0,0 +1,112 @@
+webpackHotUpdate("static\\development\\pages\\update.js",{
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/json/stringify.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/json/stringify.js":
+false,
+
+/***/ "./node_modules/define-properties/index.js":
+false,
+
+/***/ "./node_modules/function-bind/implementation.js":
+false,
+
+/***/ "./node_modules/function-bind/index.js":
+false,
+
+/***/ "./node_modules/has-symbols/shams.js":
+false,
+
+/***/ "./node_modules/has/src/index.js":
+false,
+
+/***/ "./node_modules/next/dist/lib/link.js":
+false,
+
+/***/ "./node_modules/next/link.js":
+false,
+
+/***/ "./node_modules/object-keys/index.js":
+false,
+
+/***/ "./node_modules/object-keys/isArguments.js":
+false,
+
+/***/ "./node_modules/object.assign/implementation.js":
+false,
+
+/***/ "./node_modules/object.assign/index.js":
+false,
+
+/***/ "./node_modules/object.assign/polyfill.js":
+false,
+
+/***/ "./node_modules/object.assign/shim.js":
+false,
+
+/***/ "./node_modules/prop-types-exact/build/helpers/isPlainObject.js":
+false,
+
+/***/ "./node_modules/prop-types-exact/build/index.js":
+false,
+
+/***/ "./pages/update.js":
+/*!*************************!*\
+  !*** ./pages/update.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_UpdateItem__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/UpdateItem */ "./components/UpdateItem.js");
+var _jsxFileName = "C:\\projects\\sick-fits\\frontend\\pages\\update.js";
+
+
+
+var Sell = function Sell(_ref) {
+  var query = _ref.query;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_UpdateItem__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    id: query.id,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Sell);
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/update")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=update.js.7fd6a9a078dbb2313741.hot-update.js.map
